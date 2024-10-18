a966df2202a30fc4b39206fdb68bc79f431bf1bc
Preisler <Preisler@PREISLERANDA4A3>
2024-10-18 13:43:30 +0200

    Add project files.

---
+++ PreislerDoga/Könyv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreislerDoga
+{
+    class Könyv
+    {
+        public string Cím { get; set; }
+        public string Típus { get; set; }
+
+        public static List<Könyv> könyvek = new List<Könyv>();
+        public static List<string> könyv_types = new List<string> { "Romantikus regények", "Történelmi regények", "Misztikus könyvek" };
+
+
+
+        public Könyv(string cím, string típus)
+        {
+            Cím = cím;
+            Típus = típus;
+        }
+
+        public override string ToString()
+        {
+            return Cím + " " + Típus;
+        }
+
+    }
+}
---
+++ PreislerDoga/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PreislerDoga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tipus.Items.Clear();
+            FilterTipus.Items.Clear();
+            foreach (var i in Könyv.könyv_types)
+            {
+                Tipus.Items.Add(i);
+                FilterTipus.Items.Add(i);
+            }
+        }
+
+        private void Render()
+        {
+            DisplayList.Items.Clear();
+            foreach (Könyv könyv in Könyv.könyvek)
+            {
+                DisplayList.Items.Add(könyv);
+            }
+        }
+
+        private void Filter(object sender, RoutedEventArgs e)
+        {
+            DisplayList.Items.Clear();
+            foreach (Könyv könyv in Könyv.könyvek)
+            {
+                if (könyv.Típus == FilterTipus.Text)
+                {
+                    DisplayList.Items.Add(könyv);
+                }
+            }
+            FilterTipus.Text = "";
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            String name = Cim.Text;
+            String type = Tipus.Text;
+
+            if (name == null) {
+                MessageBox.Show("Nem adtál meg címet!");
+                return;
+            }
+            if (name == "")
+            {
+                MessageBox.Show("Nem adtál meg címet!");
+                return;
+            }
+            if (type == null) {
+                MessageBox.Show("Nem adtál meg típust!");
+                return;
+            }
+            if (type == "")
+            {
+                MessageBox.Show("Nem adtál meg típust!");
+                return;
+            }
+
+            Könyv.könyvek.Add(new Könyv(name, type));
+
+            Cim.Text = "";
+            Tipus.Text = "";
+
+            Render();
+        }
+
+        private void Del(object sender, MouseButtonEventArgs e)
+        {
+
+            Könyv selectedKönyv = (Könyv)DisplayList.SelectedItem;
+
+            MessageBoxResult result = MessageBox.Show($"Biztosan törölni akarod a '{selectedKönyv.Cím}' ?", "Törlés megerősítése", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Könyv.könyvek.Remove(selectedKönyv);
+                Render();
+            }
+        }
+
+        private void ShowAll(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Összes könyv: \n" + string.Join("\n", Könyv.könyvek));
+        }
+
+        private void ResetFilter(object sender, RoutedEventArgs e)
+        {
+            Render();
+        }
+    }
+}
